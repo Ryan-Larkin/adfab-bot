a757173f81c41fd6d33f9ab93b65b959772d32c2
Alexandre Beaurain <alexandre.beaurain@adfab.fr>
2017-05-23 10:58:17 +0200

    feature: ADFABBOT-1 pre commit / post install

--- bin/crypt.sh
+++ bin/crypt.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 DIR=`dirname $0`
-if [ -z "${ENCRYPTED_KEY}" -o -z "${ENCRYPTED_IV}" ]
+if [ -f "${DIR}/variables.sh" ]
 then
-	echo "Please provide ENCRYPTED_KEY and ENCRYPTED_IV environment variables"
-	exit 1
+	if [ -z "${ENCRYPTED_KEY}" -o -z "${ENCRYPTED_IV}" ]
+	then
+		echo -e "\033[1;31mPlease provide ENCRYPTED_KEY and ENCRYPTED_IV environment variables.\033[0m"
+		echo "You'll find them inside wiki or you can generate with following command:"
+		echo -e "\033[1;32mopenssl enc -aes-256-cbc -k *secret* -P -md sha1\033[0m"
+		echo "If you generate new credentials, don't forget to copy then inside wiki AND continuous integration."
+		exit 1
+	fi
+	openssl enc -aes-256-cbc -K ${ENCRYPTED_KEY} -iv ${ENCRYPTED_IV} -in ${DIR}/variables.sh -out ${DIR}/variables.sh.enc
 fi
-openssl enc -aes-256-cbc -K ${ENCRYPTED_KEY} -iv ${ENCRYPTED_IV} -in ${DIR}/variables.sh -out ${DIR}/variables.sh.enc
--- bin/decrypt.sh
+++ bin/decrypt.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 DIR=`dirname $0`
-openssl enc -aes-256-cbc -K ${ENCRYPTED_KEY} -iv ${ENCRYPTED_IV} -in ${DIR}/variables.sh.enc -out ${DIR}/variables.sh -d
+if [ -f "${DIR}/variables.sh.enc" ]
+then
+	if [ -z "${ENCRYPTED_KEY}" -o -z "${ENCRYPTED_IV}" ]
+	then
+		echo -e "\033[1;31mPlease provide ENCRYPTED_KEY and ENCRYPTED_IV environment variables.\033[1;31m"
+		echo "You'll find them inside wiki."
+		exit 0
+	fi
+	openssl enc -aes-256-cbc -K ${ENCRYPTED_KEY} -iv ${ENCRYPTED_IV} -in ${DIR}/variables.sh.enc -out ${DIR}/variables.sh -d
+fi
